0687bcac41d80b6a76ae29faae17dd8bf35578c2
Nampoina <rnatolonampoina@gmail.com>
2020-05-16 20:11:59 +0300

    publication

--- app/APIConsumer/KrakenAPIConsumer.php
+++ app/APIConsumer/KrakenAPIConsumer.php
@@ -46,7 +46,6 @@ class KrakenAPIConsumer extends APIConsumer {
 	private function buildHeader()
 	{
 		$header = [
-			'Content-Length: 0',
 			'Accept:application/json',
 		    'Content-Type: application/json; charset=utf-8',
 		    'X-Requested-With: XMLHttpRequest'
